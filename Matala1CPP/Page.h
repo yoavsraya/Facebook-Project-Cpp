179facaeb9356740e25cb63f1eff2933fec297b1
Jonathan Brooker <jonathanbr@mta.ac.il>
2022-12-29 16:21:08 +0200

    d'tor page

--- Matala1CPP/Page.h
+++ Matala1CPP/Page.h
@@ -16,6 +16,7 @@ public:
 	page(const char* name, const char* status1, const char* status2) noexcept(false); //ct'or
 	page() {}; //empty ct'or
 	page(const string name); //ct'or
+	~page(); //d'tor
 	bool operator>(const page& _page)const; //oprator > Q2
 	bool operator>(const member& _member)const; //oprator > Q2
 	int findFollowerInd(member* const follower)const; //return member index
